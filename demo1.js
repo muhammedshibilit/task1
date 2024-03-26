4b79a80187800a77c3f8b2444a4f36d8c079e7f8
muhammedshibilit <shibilibava89@gmail.com>
2024-03-26 13:29:52 +0530

    task1

--- demo1.js
+++ demo1.js
@@ -67,70 +67,101 @@
 // displayPrimeNumbers(startInterval, endInterval);
 
 
-// higherOrder
+// // higherOrder
 
-// const radius=[1,2,3,6,8,1000];
+// // const radius=[1,2,3,6,8,1000];
 
-// //logic to calculate area
+// // //logic to calculate area
  
-// const area= function(radius){
-//     return Math.PI*radius*radius;
-// }
+// // const area= function(radius){
+// //     return Math.PI*radius*radius;
+// // }
 
-// //logic to calculate diameter
-
-
-// const diameter= function(radius){
-//     return 2*radius;
-// }
-
-// //logic to calculate circumeference
-
-// const circumeference= function(radius){
-//     return Math.PI*2*radius;
-// }
-
-// //
-// const calculate=function(radius,logic){
-//     const output=[];
-//     for(i=0; i<radius.length; i++){
-//         output.push(logic(radius[i]))
-//     }
-//     return output;
-// }
-
-// console.log(calculate(radius,area));
-// console.log(calculate(radius,diameter));
-// console.log(calculate(radius,circumeference));
+// // //logic to calculate diameter
 
 
+// // const diameter= function(radius){
+// //     return 2*radius;
+// // }
 
-// //star
+// // //logic to calculate circumeference
 
-// let n=6;
-// let m=10;
+// // const circumeference= function(radius){
+// //     return Math.PI*2*radius;
+// // }
 
-// for(let i=0; i<n; i++){
-//     console.log("*   ".repeat(m));
-// }
+// // //
+// // const calculate=function(radius,logic){
+// //     const output=[];
+// //     for(i=0; i<radius.length; i++){
+// //         output.push(logic(radius[i]))
+// //     }
+// //     return output;
+// // }
+
+// // console.log(calculate(radius,area));
+// // console.log(calculate(radius,diameter));
+// // console.log(calculate(radius,circumeference));
+
+
+
+// // //star
+
+// // let n=6;
+// // let m=10;
+
+// // for(let i=0; i<n; i++){
+// //     console.log("*   ".repeat(m));
+// // }
 
   
     
     
-// //products
+// // //products
 
-// let products=[
-//     {pid:100,pName:'apple',band:'5g',price:120000,display:'led'},
-//     {pid:101,pName:'samsaung',band:'5g',price:45000,display:'led'},
-//     {pid:102,pName:'blackberry',band:'4g',price:50000,display:'led'},
-//     {pid:103,pName:'nokia',band:'3g',price:1200,display:'lcd'},
-//     {pid:104,pName:'motorola',band:'4g',price:10000,display:'lcd'}
-// ]
+let products=[
+    {pid:100,pName:'apple',band:'5g',price:120000,display:'led'},
+    {pid:101,pName:'samsaung',band:'5g',price:45000,display:'led'},
+    {pid:102,pName:'blackberry',band:'4g',price:50000,display:'led'},
+    {pid:103,pName:'nokia',band:'3g',price:1200,display:'lcd'},
+    {pid:104,pName:'motorola',band:'4g',price:10000,display:'lcd'}
+]
 
-// //product name only
-// let product1=products.map(product1=>product1.pName)
-// console.log(product1)
+// // //product name only
+// // let product1=products.map(product1=>product1.pName)
+// // console.log(product1)
 
-// //details of product with lcd display
-// let product2=products.filter(product2=>product2.display==="lcd")
-// console.log(product2)
+// // //details of product with lcd display
+let product2=products.filter(product2=>product2.display==="lcd")
+console.log(product2)
+
+//print 5g mobile
+
+let product3=products.filter(p=>p.band==="5g")
+product3.forEach((p)=>console.log(p.price,p.pName))
+
+
+//filter mobiles based on prices 
+
+let minPrice=1000
+let maxPrice=10000
+let product4=products.filter(p=>p.price>=minPrice&&p.price<=maxPrice)
+product4.forEach((p)=>console.log(p.pName))
+
+
+// costly mobile
+
+let costlyproduct = products.reduce((minPriceproduct,currentProduct)=>{
+    return(currentProduct.price > minPriceproduct.price)? currentProduct:minPriceproduct;
+},products[0]);
+
+console.log("costly mobile:",costlyproduct.pName);
+
+
+// low cost mobile
+
+let lowcostproduct = products.reduce((maxPriceproduct,currentProduct)=>{
+    return(currentProduct.price < maxPriceproduct.price)? currentProduct:maxPriceproduct;
+},products[0]);
+
+console.log("costly mobile:",lowcostproduct.pName);
